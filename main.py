e04c96515c6ae3f06d4ef4db1f04491551748be9
Fran Ruiz <franciscojruizserrano@gmail.com>
2024-11-02 11:37:27 +0100

    Primer commit con el programa simple de hello word

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-lista = [1,2,3,4,5,6,7,8,"Pera","Pera"]
 
-elemento_eliminar = "Pera"
-
-while elemento_eliminar in lista:
-    lista.remove(elemento_eliminar)
-
-print(lista)
+p="hello world"
+print(p)
